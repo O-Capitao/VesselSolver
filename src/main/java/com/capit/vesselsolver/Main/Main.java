031dbef0ddd7485c6fd9ec9f36466cbad39b07e0
FranciscoLocal <fcc@gmail.com>
2016-08-15 15:55:54 +0100

    commited from NetBeans

--- src/main/java/com/capit/vesselsolver/Main/Main.java
+++ src/main/java/com/capit/vesselsolver/Main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.capit.vesselsolver;
+package com.capit.vesselsolver.Main;
 
 /**
  *
---
+++ src/main/java/com/capit/vesselsolver/entity/Vessel.java
@@ -0,0 +1,18 @@
+package com.capit.vesselsolver.entity;
+
+/**
+ *  Container Class for Vessel Data
+ * 
+ * @author capitaof
+ */
+public class Vessel {
+    
+    public String name;
+    
+    public double csi;
+    public double psi;
+    
+    public Vessel(String name){
+        this.name=name;
+    }
+}
